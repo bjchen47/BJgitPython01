226e9168bdccc3d7534e03750ff8e4c6849ad2da
bjchen <bjchen47@gmail.com>
2020-06-15 11:37:11 +0800

    add files Python excises 102 104

---
+++ PYa102.py
@@ -0,0 +1,9 @@
+num1=eval(input())
+num2=eval(input())
+num3=eval(input())
+num4=eval(input())
+
+print("|{:7.2f} {:7,.2f}|".format(num1,num2))
+print("|{:7.2f} {:7.2f}|".format(num3,num4))
+print("|{:<7.2f} {:<7.2f}|".format(num1,num2))
+print("|{:<7.2f} {:<7.2f}|".format(num3,num4))
---
+++ PYa104.py
@@ -0,0 +1,10 @@
+import math
+PI=math.pi
+
+radius = eval(input())
+peri=2*PI*radius
+area=PI*radius*radius
+
+print("Radius = {:0.2f}".format (radius))
+print("Perimeter = {:0.2f}".format (peri))
+print("Area = {:0.2f}".format (area))  
